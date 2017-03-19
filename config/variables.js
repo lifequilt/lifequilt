60fe97124261764c55bcb9c7f412f4bba27011d8
brianmock <briannealmock@gmail.com>
2017-03-19 03:41:59 -0500

    Update header with active nav links

--- config/variables.js
+++ config/variables.js
@@ -1,7 +1,9 @@
 var globalVariables = {
   primary: '#fff',
   orangeLight: '#F48F0D',
+  orangeMedium: '#FF9F1C',
   orangeDark: '#EF632F',
+  blueMedium: '#2E5077',
   grayLightest: 'rgb(247, 247, 247)',
   grayLight: 'rgb(236, 237, 239)',
   grayMedium: 'rgb(155, 161, 167)',
