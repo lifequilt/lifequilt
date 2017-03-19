9facdb169bac51cce9de9c2f9d76161e0a27421c
Harrison Klucher <hklucher@gmail.com>
2017-03-19 03:39:48 -0500

    remove commented code

--- src/api/apiDatabase/getDatabase.js
+++ src/api/apiDatabase/getDatabase.js
@@ -2,7 +2,6 @@ import api from '../api';
 
 const getDatabase = url => {
   const result = api.database().ref(url);
-  // const promise = result.on('value', snapshot => { console.log(snapshot.val()); return snapshot.val(); });
   return result;
 };
 
