38e12c98eb72b091299b019a4810e8ec3cdc9b98
brianmock <briannealmock@gmail.com>
2017-03-18 21:03:37 -0500

    Add support options, small styling updates

--- src/pages/DonatePage/DonatePage.js
+++ src/pages/DonatePage/DonatePage.js
@@ -18,30 +18,32 @@ class DonatePage extends Component {
     return (
       <div className={styles.DonatePage}>
         <h2>Donate</h2>
-        <Section>
-          Donate towards these items
-        </Section>
 
         <Section>
           <DonateForm />
         </Section>
 
         <div className={styles.supportContainer}>
-          <SupportCard
-            supportImage="/public/backpack-pic.jpg"
-            supportTitle="Matching Gift"
-            supportDescription="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum "
-          />
-          <SupportCard
-            supportImage="/public/backpack-pic.jpg"
-            supportTitle="Amazon Smile"
-            supportDescription="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum "
-          />
-          <SupportCard
-            supportImage="/public/backpack-pic.jpg"
-            supportTitle="Follow on Facebook"
-            supportDescription="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum "
-          />
+          <div className={styles.supportHeader}>
+            Other ways to support us
+          </div>
+          <div className={styles.supportCardContainer}>
+            <SupportCard
+              supportImage="/public/backpack-pic.jpg"
+              supportTitle="Matching Gift"
+              supportDescription="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum "
+            />
+            <SupportCard
+              supportImage="/public/backpack-pic.jpg"
+              supportTitle="Amazon Smile"
+              supportDescription="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum "
+            />
+            <SupportCard
+              supportImage="/public/backpack-pic.jpg"
+              supportTitle="Follow on Facebook"
+              supportDescription="Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum "
+            />
+          </div>
         </div>
 
         <Section>
