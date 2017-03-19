8af5553510733a22c116f20ea5b5952ec45c3ca0
Lorena Dela Cruz <dclorena11@gmail.com>
2017-03-19 03:26:28 -0500

    Style partner form

--- src/pages/Participate/Partner/Partner.jsx
+++ src/pages/Participate/Partner/Partner.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import { UserForm } from '../../../forms/user/user-form';
 
+const styles = require('./Partner.css');
+
 export class Partner extends Component {
   render() {
     return (
-      <div>
-        <h2>Partner</h2>
-        <UserForm
-          userType="partner"
-          history={this.props.history}
-        />
+      <div className={styles.container}>
+        <div className={styles.form}>
+          <h2>Partner</h2>
+          <UserForm
+            userType="partner"
+            history={this.props.history}
+          />
+        </div>
+        <div className={styles.sidebar}>
+          <h2>This is sidebar content.</h2>
+          <p>Picture Here</p>
+          <p>Picture Here</p>
+          <p>Picture Here</p>
+        </div>
       </div>
     );
   }
