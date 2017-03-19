9a8b84014e4a4f400ba7877b57230e669b15217b
brianmock <briannealmock@gmail.com>
2017-03-19 04:29:03 -0500

    Update participation page

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import classnames from 'classnames';
 
 const styles = require('./Banner.css');
 
-const Banner = props => (
-  <div className={styles.banner} >
-    {props.children}
-  </div>
-);
+const Banner = props => {
+  const bannerClasses = classnames(styles.banner, props.className);
+
+  return (
+    <div className={bannerClasses} >
+      {props.children}
+    </div>
+  );
+};
 
 export default Banner;
