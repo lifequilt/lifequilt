6b01080331a3847ccb426ad43b503d3f81131dd6
Dana Sweet <sweet@jellyvision.com>
2017-03-19 04:41:16 -0500

    css tweaks and donate page updates

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const styles = require('./Banner.css');
 
 const Banner = props => (
-  <Link to="/participate">
-    <div className={styles.banner} >
-      {props.children}
-    </div>
-  </Link>
+  <div className={styles.banner} >
+    {props.children}
+  </div>
 );
 
 export default Banner;
