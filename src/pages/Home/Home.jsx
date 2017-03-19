f3157ad7cc527ddf746ae00a45003f5b45c083ca
dana sweet <dsweet100@gmail.com>
2017-03-19 01:58:37 -0500

    Swap pics (#22)
    * swap out home page pictures and add text to banner
    
    * update banner image on homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 import Banner from '../../components/Banner';
 import Section from '../../components/Section';
 
-const homepageBanner = require('../../assets/homepageBanner.jpg');
-const image1 = require('../../assets/king-quote.jpg');
-const image2 = require('../../assets/king-quote.jpg');
-const image3 = require('../../assets/king-quote.jpg');
+const homepageBanner = require('../../assets/homepageBanner_text.jpg');
+const rocketImage = require('../../assets/rocket_pencils.jpeg');
+const clothingImage = require('../../assets/winter_clothing.jpeg');
+const parentImage = require('../../assets/parent_reading.jpeg');
 const styles = require('./Home.css');
 
 const Home = () => (
@@ -14,19 +14,19 @@ const Home = () => (
     <img className={styles.homepageBanner} src={homepageBanner} alt="Banner" />
     <div className={styles.main}>
       <Section>
-        <img src={image1} alt="pic" />
+        <img src={parentImage} alt="Parent reading to child" />
         <p>
           Life Quilt is committed to breaking the barriers to education caused by poverty and lack of funds.
         </p>
       </Section>
       <Section>
-        <img src={image2} alt="pic" />
+        <img src={clothingImage} alt="Winter clothing" />
         <p>
           In partnership with other organizations, we provide school supplies, winter clothing and food to students in need.
         </p>
       </Section>
       <Section>
-        <img src={image3} alt="pic" />
+        <img src={rocketImage} alt="School supplies" />
         <p>
           We believe that parents and guardians are a child’s first teacher and Life Quilt works to provide resources they need to nurture the child’s successful academic career.
         </p>
